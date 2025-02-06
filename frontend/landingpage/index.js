6a2727232b3036291ddeeb255fe10dce60e8d3f5
amarachi okeke <ashleyjonathan555@icloud.com>
2025-02-06 13:39:19 +0000

    committing changes

--- frontend/landingpage/index.js
+++ frontend/landingpage/index.js
@@ -1,1 +1,22 @@
-console.log("Slot-ify page loaded");
+const text = "Get things done with our fast-tracking scheduling app!";
+const typingTextElement = document.getElementById('typing-text');
+let index = 0;
+const typingSpeed = 100;
+
+function typeText() {
+    if (index < text.length) {
+        typingTextElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeText, typingSpeed);
+    } else {
+        setTimeout(clearText, 2000); // Pause for 2 seconds before resetting
+    }
+}
+
+function clearText() {
+    typingTextElement.textContent = '';
+    index = 0;
+    typeText();
+}
+
+typeText();
