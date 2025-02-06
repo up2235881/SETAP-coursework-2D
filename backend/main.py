0c298dc43a6cef8abdb59093833bc5a601b89d87
polinaandrejeva03 <up2208402@myport.ac.uk>
2025-02-06 13:57:09 +0000

    backend

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+class availability():
+   
+    def __init__(self): 
+        pass
+
+class Meeting(): 
+
+    def __init__(self): 
+        pass
+
+class rooms_participant():
+
+    def __init__(self):
+        pass
