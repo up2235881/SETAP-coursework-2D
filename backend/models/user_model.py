a9bba54e8b3a16f441ed69f21315f3fb80cc21a6
lisaruwona <lisafaithruwona@gmail.com>
2025-02-27 13:39:14 +0000

    commiting changes

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -2,9 +2,6 @@ class User:
     def __init__(self, user_id, username, first_name, middle_name, surname, email, password):
         self.user_id = user_id
         self.username = username
-        self.first_name = first_name
-        self.middle_name = middle_name
-        self.surname = surname
         self.email = email
         self.password = password
 
@@ -13,8 +10,6 @@ class User:
         return {
             "user_id" : self.user_id,
             "username" : self.username,
-            "first name" : self.first_name,
-            "surname" : self.surname,
             "email" : self.email,
             "password" : self.password
         }
