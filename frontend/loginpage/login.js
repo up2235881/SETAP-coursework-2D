78361589b43cf2b7e468d61766dd6d000a8a892a
Lisa <lisafaithruwona@gmail.com>
2025-03-20 12:27:04 +0000

    commiting changes

--- frontend/loginpage/login.js
+++ frontend/loginpage/login.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', async (event) =
     const studentEmailUsername = document.getElementById('student-username-email').value;
     const password = document.getElementById('password').value;
     try{
-        const response = await fetch ('/login', { 
+        const response = await fetch ('http://localhost:3000/login', { 
             method: "POST",
             headers: {
                 'Content-type' : 'application/json',
